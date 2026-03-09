f655b3b683641c5cd88b5998000a52dc408d7589
dreamboy-ishak <creatorishak.sk@gmail.com>
2026-03-09 13:05:37 +0600

    Add practice 1: ViewBag example

---
+++ Asp-Net_MVC/ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewBag.Models;
+
+namespace ViewBag.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult ViewBagExample(){
+        ViewBag.Name = "Ishak";
+        ViewBag.Age = 22;
+        return View();
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
