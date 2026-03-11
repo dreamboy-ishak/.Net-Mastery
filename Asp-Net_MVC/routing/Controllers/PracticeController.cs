05722442ba09c47ae132fc2b585053ddda68f28d
dreamboy-ishak <creatorishak.sk@gmail.com>
2026-03-11 11:53:49 +0600

    Add routing practices

---
+++ Asp-Net_MVC/routing/Controllers/PracticeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class AmarController : Controller
+{
+    public IActionResult Index(){
+        return Content("This is a index page");
+    }
+
+    public IActionResult Hello(){
+        return Content("This is a MVC app!");
+    }
+
+    public IActionResult Contact(){
+        return Content("This is a Contact page");
+    }
+
+    //Understand how url maps to controller actions
+
+}
